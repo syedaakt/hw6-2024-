51b6ab462348ba0bd4b4d312f3dff5890c1af219
syedaakt <113367460+syedaakt@users.noreply.github.com>
2024-04-03 11:24:13 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,4 +1,5 @@
 var video;
+let slowDownCount = 0;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
@@ -9,21 +10,26 @@ window.addEventListener("load", function() {
 	console.log("autoplay is set to" + video.loop);
 });
 
+// Play Button
 document.querySelector("#play").addEventListener("click", function() { 	
 	video.play();
   	console.log("Play Video");
+	document.querySelector("#volume").innerHTML = 100*(video.volume)+"%";
  });
 
+ //Pause button
  document.querySelector("#pause").addEventListener("click", function() {
  	video.pause();
  	console.log("Pause Video");
  });
 
+ //Slow down button
  document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate *= .9;
 	console.log("New video speed (slowed down): " + video.playbackRate);
 });
 
+//Speed up button
 document.querySelector("#faster").addEventListener("click", function() {
 
 	video.playbackRate /= .9;
@@ -31,18 +37,21 @@ document.querySelector("#faster").addEventListener("click", function() {
 
 });
 
+//Skip Ahead button
 document.querySelector("#skip").addEventListener("click", function() {
 	const newTime = Math.min(video.currentTime + 10, video.duration);
 	video.currentTime = newTime;
 	console.log('New video location:', video.currentTime);
 });
 
+//Mute button, must turn into unmute
 document.querySelector("#mute").addEventListener("click", function() {
 	video.muted = !video.muted;
 	document.querySelector("#mute").textContent = video.muted? 'Unmute' : 'Mute';
 	console.log("video is " + video.muted );
 });
 
+//Volume slider and volume ID
 document.querySelector("#slider").addEventListener("change", function() {
 	current = document.querySelector("#slider").value/100;
 	video.volume = current;
@@ -50,8 +59,14 @@ document.querySelector("#slider").addEventListener("change", function() {
 	console.log("current volume" + video.volume);
 });
 
+//Old Scool button
 document.querySelector("#vintage").addEventListener("click", function() {
 	video.classList.add('oldSchool');
+});
+
+//Original button
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove('oldSchool');
 });
 
 document.querySelector("#orig").addEventListener("click", function() {
